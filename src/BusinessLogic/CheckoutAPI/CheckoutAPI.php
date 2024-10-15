9a0ec89d0be56f0c3ce9b3c8ba338fe0fd7a3d7a
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-10-15 16:38:43 +0200

    Implement Checkout API for checkout

--- src/BusinessLogic/CheckoutAPI/CheckoutAPI.php
+++ src/BusinessLogic/CheckoutAPI/CheckoutAPI.php
@@ -3,7 +3,9 @@
 namespace Unzer\Core\BusinessLogic\CheckoutAPI;
 
 use Unzer\Core\BusinessLogic\ApiFacades\Aspects\ErrorHandlingAspect;
+use Unzer\Core\BusinessLogic\ApiFacades\Aspects\StoreContextAspect;
 use Unzer\Core\BusinessLogic\Bootstrap\Aspect\Aspects;
+use Unzer\Core\BusinessLogic\CheckoutAPI\PaymentMethods\Controller\CheckoutPaymentMethodsController;
 
 /**
  * Class AdminAPI. Integrations should use this class for communicating with Admin API.
@@ -22,5 +24,18 @@ class CheckoutAPI
     public static function get(): object
     {
         return Aspects::run(new ErrorHandlingAspect())->beforeEachMethodOfInstance(new CheckoutAPI());
+    }
+
+    /**
+     * @param string $storeId
+     *
+     * @return CheckoutPaymentMethodsController
+     */
+    public function connection(string $storeId): object
+    {
+        return Aspects
+            ::run(new ErrorHandlingAspect())
+            ->andRun(new StoreContextAspect($storeId))
+            ->beforeEachMethodOfService(CheckoutPaymentMethodsController::class);
     }
 }
