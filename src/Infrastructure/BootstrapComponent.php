b9ca59d84f6e2fd0c36868c2208e91d23faa4a4d
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-10-30 16:01:47 +0100

    Update bootstrap

--- src/Infrastructure/BootstrapComponent.php
+++ src/Infrastructure/BootstrapComponent.php
@@ -5,6 +5,16 @@ namespace Unzer\Core\Infrastructure;
 use Unzer\Core\Infrastructure\Configuration\ConfigurationManager;
 use Unzer\Core\Infrastructure\Http\CurlHttpClient;
 use Unzer\Core\Infrastructure\Http\HttpClient;
+use Unzer\Core\Infrastructure\TaskExecution\AsyncProcessStarterService;
+use Unzer\Core\Infrastructure\TaskExecution\Events\QueueItemStateTransitionEventBus;
+use Unzer\Core\Infrastructure\TaskExecution\Interfaces\AsyncProcessService;
+use Unzer\Core\Infrastructure\TaskExecution\Interfaces\TaskRunnerManager;
+use Unzer\Core\Infrastructure\TaskExecution\Interfaces\TaskRunnerStatusStorage;
+use Unzer\Core\Infrastructure\TaskExecution\Interfaces\TaskRunnerWakeup;
+use Unzer\Core\Infrastructure\TaskExecution\QueueService;
+use Unzer\Core\Infrastructure\TaskExecution\RunnerStatusStorage;
+use Unzer\Core\Infrastructure\TaskExecution\TaskRunner;
+use Unzer\Core\Infrastructure\TaskExecution\TaskRunnerWakeupService;
 use Unzer\Core\Infrastructure\Utility\Events\EventBus;
 use Unzer\Core\Infrastructure\Utility\GuidProvider;
 use Unzer\Core\Infrastructure\Utility\TimeProvider;
@@ -60,6 +70,62 @@ class BootstrapComponent
             HttpClient::CLASS_NAME,
             function () {
                 return new CurlHttpClient();
+            }
+        );
+
+        ServiceRegister::registerService(
+            EventBus::CLASS_NAME,
+            function () {
+                return EventBus::getInstance();
+            }
+        );
+
+        ServiceRegister::registerService(
+            AsyncProcessService::CLASS_NAME,
+            function () {
+                return AsyncProcessStarterService::getInstance();
+            }
+        );
+
+        ServiceRegister::registerService(
+            QueueService::CLASS_NAME,
+            function () {
+                return new QueueService();
+            }
+        );
+
+        ServiceRegister::registerService(
+            TaskRunnerWakeup::CLASS_NAME,
+            function () {
+                return new TaskRunnerWakeupService();
+            }
+        );
+
+        ServiceRegister::registerService(
+            TaskRunner::CLASS_NAME,
+            function () {
+                return new TaskRunner();
+            }
+        );
+
+        ServiceRegister::registerService(
+            TaskRunnerStatusStorage::CLASS_NAME,
+            function () {
+                return new RunnerStatusStorage();
+            }
+        );
+
+        ServiceRegister::registerService(
+            TaskRunnerManager::CLASS_NAME,
+            function () {
+                return new TaskExecution\TaskRunnerManager();
+            }
+        );
+
+        ServiceRegister::registerService(
+            QueueItemStateTransitionEventBus::CLASS_NAME,
+            function () {
+                return QueueItemStateTransitionEventBus::getInstance();
             }
         );
     }
