21e4a713a9f8ee17e50e780697547712b2e3eff1
Aleksandar Sasa Boljanovic <aleksandar.boljanovic@logeecom.com>
2024-11-07 17:33:01 +0100

    Fix logo url when it is empty
    ISSUE: UNZERCSI-57

--- Resources/admin-ui/src/components/file-upload-component/FileUploadComponent.js
+++ Resources/admin-ui/src/components/file-upload-component/FileUploadComponent.js
@@ -15,62 +15,60 @@
  * @constructor
  */
 const FileUploadComponent = ({ label = '', description = '', className = '', onFileSelect, onChange, value }) => {
-  const { elementGenerator: generator } = Unzer;
-  const cssClass = ['unzer-file-upload-box'];
-  className && cssClass.push(className);
+    const { elementGenerator: generator } = Unzer;
+    const cssClass = ['unzer-file-upload-box'];
+    className && cssClass.push(className);
 
-  const inputElement = generator.createElement('input', 'unzer-file-upload-input', '', {
-    type: 'text',
-    placeholder: '',
-    value: value,
-  });
+    const inputElement = generator.createElement('input', 'unzer-file-upload-input', '', {
+        type: 'text',
+        placeholder: '',
+        value: value,
+    });
 
-  const fileInputElement = generator.createElement('input', 'unzer-file-input-field', '', {
-    type: 'file',
-    accept: 'image/*',
-    id: "unzer-file-input",
-  });
+    const fileInputElement = generator.createElement('input', 'unzer-file-input-field', '', {
+        type: 'file',
+        accept: 'image/*',
+        id: "unzer-file-input",
+    });
 
-  onFileSelect && fileInputElement.addEventListener('change', (event) => {
-    const file = event.target.files[0];
+    onFileSelect && fileInputElement.addEventListener('change', (event) => {
+        const file = event.target.files[0];
 
-    if (file) {
-      inputElement.value = file.name;
-      onFileSelect(file);
-    }
-  });
+        if (file) {
+            inputElement.value = file.name;
+            onFileSelect(file);
+        }
+    });
 
-  onChange && inputElement.addEventListener('input', (event) => {
-    const url = event.target.value;
-    if (url) {
-      onChange(url);
-    }
-  });
+    onChange && inputElement.addEventListener('input', (event) => {
+        const url = event.target.value;
+        onChange(url);
+    });
 
-  const iconElement = generator.createElement('span', 'unzer-file-upload-icon', '', [], [
-    generator.createElementFromHTML(Unzer.imagesProvider.uploadIcon)
-  ]);
+    const iconElement = generator.createElement('span', 'unzer-file-upload-icon', '', [], [
+        generator.createElementFromHTML(Unzer.imagesProvider.uploadIcon)
+    ]);
 
-  iconElement.addEventListener('click', () => {
-    fileInputElement.click();
-  })
+    iconElement.addEventListener('click', () => {
+        fileInputElement.click();
+    })
 
-  const inputLabel = generator.createElement('label', 'unzer-label-input-label', label, { for: inputElement.id });
-  const descriptionDiv = generator.createElement('span', 'unzer-dropdown-description', description, [], []);
+    const inputLabel = generator.createElement('label', 'unzer-label-input-label', label, { for: inputElement.id });
+    const descriptionDiv = generator.createElement('span', 'unzer-dropdown-description', description, [], []);
 
-  const inputWrapper = generator.createElement('div', 'unzer-file-upload-wrapper', '', [], [
-    inputElement,
-    inputLabel,
-    fileInputElement,
-    iconElement
-  ]);
+    const inputWrapper = generator.createElement('div', 'unzer-file-upload-wrapper', '', [], [
+        inputElement,
+        inputLabel,
+        fileInputElement,
+        iconElement
+    ]);
 
-  return generator.createElement('div', cssClass.join(' '), '', null, [
-    inputWrapper,
-    descriptionDiv
-  ]);
+    return generator.createElement('div', cssClass.join(' '), '', null, [
+        inputWrapper,
+        descriptionDiv
+    ]);
 };
 
 Unzer.components.FileUploadComponent = {
-  create: FileUploadComponent
+    create: FileUploadComponent
 };
