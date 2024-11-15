eb641fd5fffd432aad127f682fccbc6b66ec065d
Ivana Nestorovic <ivana.nestorovic@logeecom.com>
2024-11-15 10:47:58 +0100

    Configuration repositories and services modifications Payment page settings for V2
    ISSUE: UNZERCR01I-9

--- tests/BusinessLogic/Domain/Disconnect/Services/DisconnectServiceTest.php
+++ tests/BusinessLogic/Domain/Disconnect/Services/DisconnectServiceTest.php
@@ -237,8 +237,8 @@ class DisconnectServiceTest extends BaseTestCase
         // arrange
         $settings = new PaymentPageSettings(
             new UploadedFile('https://www.test.com/'),
+            new UploadedFile(null),
             TranslationCollection::fromArray([['locale'=>'default','value'=>'shop'], ['locale'=>'en_us','value'=>'shop']]),
-            new TranslationCollection(new Translation('en_us',"description")),
         );
 
         $settingsEntity = new PaymentPageSettingsEntity();
