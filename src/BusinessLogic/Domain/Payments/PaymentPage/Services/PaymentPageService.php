90689688499cb08262dfcbe8024e2d833af6827b
Kristina <kristina.mutavdzic@logeecom.com>
2026-04-09 09:42:16 +0200

    Append locale to PayPage redirect URL
    ISSUE: UPP-425

--- src/BusinessLogic/Domain/Payments/PaymentPage/Services/PaymentPageService.php
+++ src/BusinessLogic/Domain/Payments/PaymentPage/Services/PaymentPageService.php
@@ -88,6 +88,9 @@ class PaymentPageService
             ),
         );
 
+        $locale = $context->getLocale() === 'default' ? 'auto' : $context->getLocale();
+        $payPageResponse->setRedirectUrl($payPageResponse->getRedirectUrl() . '&locale=' . $locale);
+
         $this->updateTransactionHistory($context);
 
         return $payPageResponse;
@@ -155,7 +158,7 @@ class PaymentPageService
      *
      * @return void
      */
-    protected function updateTransactionHistory(PaymentPageCreateContext $context) : void
+    protected function updateTransactionHistory(PaymentPageCreateContext $context): void
     {
         $transactionHistory = $this->transactionHistoryService->getTransactionHistoryByOrderId($context->getOrderId())
             ?? new TransactionHistory(
@@ -177,7 +180,8 @@ class PaymentPageService
      */
     private function getEnabledPaymentMethodSettings(PaymentPageCreateContext $context): PaymentMethodConfig
     {
-        $settings = $this->paymentMethodService->getPaymentMethodConfigByType($context->getPaymentMethodType(), $context->getSystemBookingMethod());
+        $settings = $this->paymentMethodService->getPaymentMethodConfigByType($context->getPaymentMethodType(),
+            $context->getSystemBookingMethod());
         if (!$settings || !$settings->isEnabled()) {
             throw new PaymentConfigNotFoundException(
                 new TranslatableLabel(
@@ -197,7 +201,7 @@ class PaymentPageService
      * @throws ConnectionSettingsNotFoundException
      * @throws UnzerApiException
      */
-    private function buildResources(PaymentPageCreateContext $context) : Resources
+    private function buildResources(PaymentPageCreateContext $context): Resources
     {
         $customer = $this->customerFactory->create($context);
         if ($customer !== null) {
@@ -205,7 +209,7 @@ class PaymentPageService
         }
 
         $basket = $this->basketFactory->create($context);
-        if($basket !== null) {
+        if ($basket !== null) {
             $basket = $this->unzerFactory->makeUnzerAPI()->createBasket($basket);
         }
 
